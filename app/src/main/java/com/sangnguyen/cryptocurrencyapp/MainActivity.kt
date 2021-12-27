11c0b2de023b8006daff72fa1a2c5f89431e97d9
Sang Nguyen <sangnguyendinh24work@gmail.com>
2021-12-27 17:25:46 +0700

    [c] Starter code

--- app/src/main/java/com/sangnguyen/cryptocurrencyapp/MainActivity.kt
+++ app/src/main/java/com/sangnguyen/cryptocurrencyapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.plcoding.cryptocurrencyappyt
+package com.sangnguyen.cryptocurrencyapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,7 +8,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.plcoding.cryptocurrencyappyt.ui.theme.CryptocurrencyAppYTTheme
+import com.sangnguyen.cryptocurrencyapp.ui.theme.CryptocurrencyAppYTTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +16,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             CryptocurrencyAppYTTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    
                 }
             }
         }
--- app/src/test/java/com/sangnguyen/cryptocurrencyapp/ExampleUnitTest.kt
+++ app/src/test/java/com/sangnguyen/cryptocurrencyapp/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.plcoding.cryptocurrencyappyt
+package com.sangnguyen.cryptocurrencyapp
 
 import org.junit.Test
 
