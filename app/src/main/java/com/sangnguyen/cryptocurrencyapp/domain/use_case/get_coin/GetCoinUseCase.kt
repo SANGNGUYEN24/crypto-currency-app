d20160d7b03e33992938aa5a29efa211c5b55c1a
Sang Nguyen <sangnguyendinh24work@gmail.com>
2021-12-28 11:18:40 +0700

    [f] Implement usecase, view model, dependency injection

--- app/src/main/java/com/sangnguyen/cryptocurrencyapp/domain/use_case/get_coin/GetCoinUseCase.kt
+++ app/src/main/java/com/sangnguyen/cryptocurrencyapp/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -1,8 +1,10 @@
-package com.sangnguyen.cryptocurrencyapp.domain.use_case.get_coins
+package com.sangnguyen.cryptocurrencyapp.domain.use_case.get_coin
 
 import com.sangnguyen.cryptocurrencyapp.common.Resource
 import com.sangnguyen.cryptocurrencyapp.data.remote.data_transfer_object.toCoin
+import com.sangnguyen.cryptocurrencyapp.data.remote.data_transfer_object.toCoinDetail
 import com.sangnguyen.cryptocurrencyapp.domain.model.Coin
+import com.sangnguyen.cryptocurrencyapp.domain.model.CoinDetail
 import com.sangnguyen.cryptocurrencyapp.domain.repository.CoinRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -10,12 +12,12 @@ import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
 
-class GetCoinsUseCase @Inject constructor(private val repository: CoinRepository) {
-    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+class GetCoinUseCase @Inject constructor(private val repository: CoinRepository) {
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow {
         try {
             emit(Resource.Loading())
-            val coins = repository.getCoins().map{it.toCoin()}
-            emit(Resource.Success(coins))
+            val coin = repository.getCoinById(coinId).toCoinDetail()
+            emit(Resource.Success(coin))
         } catch (e: HttpException) {
             emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
         } catch (e: IOException) {
--- app/src/main/java/com/sangnguyen/cryptocurrencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/sangnguyen/cryptocurrencyapp/presentation/MainActivity.kt
@@ -5,13 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import com.sangnguyen.cryptocurrencyapp.presentation.ui.theme.CryptocurrencyAppYTTheme
+import com.sangnguyen.cryptocurrencyapp.presentation.ui.theme.CryptocurrencyAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CryptocurrencyAppYTTheme {
+            CryptocurrencyAppTheme {
                 Surface(color = MaterialTheme.colors.background) {
                 }
             }
